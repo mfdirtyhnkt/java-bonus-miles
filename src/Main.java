9297bfeea6c724ef5a958229da959514b2c82189
Neto Georgiy <dirtyhnkt@gmail.com>
2025-10-03 19:48:19 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int ticketPrice = 14356;
-        int rublesFor1Mile= 20;
+        int rublesFor1Mile = 20;
 
 
-        int bonusMiles =ticketPrice / rublesFor1Mile;
+        int bonusMiles = ticketPrice / rublesFor1Mile;
 
         System.out.println("Количество начисленных миль: " + bonusMiles);
     }
